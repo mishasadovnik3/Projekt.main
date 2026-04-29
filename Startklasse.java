b576568b5b29c001c1e974a9313bb534c9c7e167
mishasadovnik3 <misha.sadovnik3@gmail.com>
2026-04-29 11:52:52 +0200

    Startklasse.java

---
+++ Startklasse.java
@@ -0,0 +1,10 @@
+package paket_J540;
+
+public class StartKlasse
+{
+  public static void main(String[] args)
+	 { // Objekt deklarieren und erzeugen
+	  HauptFenster fenster1 = new HauptFenster();
+      fenster1.setVisible(true);
+	 }
+}
